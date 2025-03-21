07d51420d2788c19d65bab902fd409c9474afd67
limon-chandra-ray <limonroy.19cse013@gmail.com>
2025-03-22 03:28:41 +0600

    'update-this-again2'

--- server.js
+++ server.js
@@ -73,13 +73,14 @@ async function run() {
         app.get('/api/appoinments',async(req,res)=>{
             try{
                 // Extract the query parameter for filtering
-                const { appoinment_status } = req.query;
+                const { appoinment_status, agent_email } = req.query;
 
-                // Build the match filter dynamically
-                const matchStage = {
-                    $match: appoinment_status ? { appoinment_status } : {}
-                };
-   
+                
+                const matchStage = { $match: {} };
+
+                if (appoinment_status) matchStage.$match.appoinment_status = appoinment_status;
+                if (agent_email) matchStage.$match.agent_email = agent_email;
+                
                 const result=await AppoinmentCollection.aggregate([
                     matchStage,
                     {
